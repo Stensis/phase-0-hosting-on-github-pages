b58bff75da826cd316de50a7f4656ca6f8e3bf2c
stensis <irenenjuguna98@gmail.com>
2022-06-12 14:35:12 +0300

    add script.js, connect to index.html

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by Javascript";
-document.querySelector("body").appearChild(h2)
+<script type="text/javascript">
+    var name=prompt(what is your name )
+alert("Hello "+name);
+</script>
